1b37b68b1c169ef31d82ae409713e63482224e91
sflcode <sfl.code@gmail.com>
2018-04-05 17:19:20 -0300

    Add timeout and make webdriver update and run as daemon on run

--- e2e/hooks/hooks.ts
+++ e2e/hooks/hooks.ts
@@ -2,7 +2,7 @@ const { BeforeAll, After, Status, setDefaultTimeout } = require('cucumber');
 import { browser } from 'protractor';
 
 
-setDefaultTimeout(10 * 1000);
+setDefaultTimeout(30 * 1000);
 
 BeforeAll(()=>{
   console.log('before all');
